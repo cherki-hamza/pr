5a007a8259d474909a584597b2f04b021294c239
cherki-hamza <cherki0hamza@gmail.com>
2024-08-19 13:13:21 +0400

    latest update for pr content 08-19-2024 .

--- database/seeders/SiteSeeder.php
+++ database/seeders/SiteSeeder.php
@@ -97,6 +97,23 @@ class SiteSeeder extends Seeder
         $sites = collect($data);
 
         foreach($sites as $site){
+
+            if($site['site_domain_authority'] == ''){
+                $site_domain_authority = 0;
+            }elseif($site['site_domain_authority'] == null){
+                $site_domain_authority = 0;
+            }else{
+                $site_domain_authority = intval($site['site_domain_authority']);
+            }
+
+            if($site['site_domain_rating'] == ''){
+                $site_domain_rating = 0;
+            }elseif($site['site_domain_rating'] == null){
+                $site_domain_rating = 0;
+            }else{
+                $site_domain_rating = intval($site['site_domain_rating']);
+            }
+
             $site_region_location = Helper::stringToNumeric($site['site_monthly_traffic']);
             Site::create([
                 'user_id'   => 1,
@@ -107,12 +124,10 @@ class SiteSeeder extends Seeder
                 'site_region_location' => $site['site_region_location'],
                 'site_monthly_traffic' => $site_region_location,
                 'site_language' => $site['language'],
-
-                'site_domain_authority' => $site['site_domain_authority'],
-                'site_domain_rating' => $site['site_domain_rating'],
+                'site_domain_authority' => $site_domain_authority,   // $site['site_domain_authority']
+                'site_domain_rating' => $site_domain_rating,   // $site['site_domain_rating']
                 'site_sposored' => $site['site_sposored'],
                 'site_indexed' => $site['site_indexed'],
-
                 'site_dofollow' => $site['site_dofollow'],
                 'site_images' => 'No',
                 'site_time' => $site['site_time'],
@@ -134,25 +149,43 @@ class SiteSeeder extends Seeder
 
 
         foreach($sites2 as $site2){
+
+            if($site2['site_domain_authority'] == ''){
+                $site_domain_authority = 0;
+            }elseif($site2['site_domain_authority'] == null){
+                $site_domain_authority = 0;
+            }else{
+                $site_domain_authority = intval($site2['site_domain_authority']);
+            }
+
+            if($site2['site_domain_rating'] == ''){
+                $site_domain_rating = 0;
+            }elseif($site2['site_domain_rating'] == null){
+                $site_domain_rating = 0;
+            }else{
+                $site_domain_rating = intval($site2['site_domain_rating']);
+            }
+
+
             Site::create([
                 'user_id'   => 1,
-                'site_name' => $site['site_name'],
-                'site_url' => preg_replace("(^https?://)", "", $site['site_url']),
-                'site_category' => $site['site_category'],
-                'site_price' => $site['site_price'],
-                'site_region_location' => $site['site_region_location'],
-                'site_monthly_traffic' => $site['site_monthly_traffic'],
-                'site_language' => $site['language'],
+                'site_name' => $site2['site_name'],
+                'site_url' => preg_replace("(^https?://)", "", $site2['site_url']),
+                'site_category' => $site2['site_category'],
+                'site_price' => $site2['site_price'],
+                'site_region_location' => $site2['site_region_location'],
+                'site_monthly_traffic' => $site2['site_monthly_traffic'],
+                'site_language' => $site2['language'],
 
-                'site_domain_authority' => $site['site_domain_authority'],
-                'site_domain_rating' => $site['site_domain_rating'],
-                'site_sposored' => $site['site_sposored'],
-                'site_indexed' => $site['site_indexed'],
+                'site_domain_authority' => $site_domain_authority, // $site2['site_domain_authority']
+                'site_domain_rating' => $site_domain_rating,  // $site2['site_domain_rating']
+                'site_sposored' => $site2['site_sposored'],
+                'site_indexed' => $site2['site_indexed'],
 
-                'site_dofollow' => $site['site_dofollow'],
+                'site_dofollow' => $site2['site_dofollow'],
                 'site_images' => 'No',
-                'site_time' => $site['site_time'],
-                'spam_score' => $site['spam_score'] ?? '-',
+                'site_time' => $site2['site_time'],
+                'spam_score' => $site2['spam_score'] ?? '-',
                 'word_limite' =>  0,
             ]);
         }
@@ -172,24 +205,41 @@ class SiteSeeder extends Seeder
         //return $sites[0]['site_name'];
 
         foreach($sites3 as $site3){
+
+            if($site3['site_domain_authority'] == ''){
+                $site_domain_authority = 0;
+            }elseif($site3['site_domain_authority'] == null){
+                $site_domain_authority = 0;
+            }else{
+                $site_domain_authority = intval($site3['site_domain_authority']);
+            }
+
+            if($site3['site_domain_rating'] == ''){
+                $site_domain_rating = 0;
+            }elseif($site3['site_domain_rating'] == null){
+                $site_domain_rating = 0;
+            }else{
+                $site_domain_rating = intval($site3['site_domain_rating']);
+            }
+
             Site::create([
                 'user_id'   => 1,
                 'site_name' => $site3['site_name'],
-                'site_url' => preg_replace("(^https?://)", "", $site['site_url']),
+                'site_url' => preg_replace("(^https?://)", "", $site3['site_url']),
                 'site_category' => $site3['site_category'],
                 'site_price' => $site3['site_price'],
                 'site_region_location' => $site3['site_region_location'],
                 'site_monthly_traffic' => $site3['site_monthly_traffic'],
 
-                'site_domain_authority' => $site3['site_domain_authority'],
-                'site_domain_rating' => $site3['site_domain_rating'],
+                'site_domain_authority' => $site_domain_authority,  // $site3['site_domain_authority']
+                'site_domain_rating' => $site_domain_rating, // $site3['site_domain_rating']
                 'site_sposored' => $site3['site_sposored'],
                 'site_indexed' => $site3['site_indexed'],
 
                 'site_dofollow' => $site3['site_dofollow'],
                 'site_images' => $site3['site_images'],
                 'site_time' => $site3['site_time'],
-                'spam_score' => $site['spam_score'] ?? '-',
+                'spam_score' => $site3['spam_score'] ?? '-',
             ]);
         }
 
